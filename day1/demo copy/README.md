9a7b4357be45f440d2c594c0cde5e47a0b25cee8
leechaeing <alee27@risd.edu>
2026-06-30 11:20:39 +0100

    organizing day 1 demo copy

---
+++ day1/demo copy/README.md
@@ -0,0 +1,13 @@
+# Title: Practice Repository for Git
+
+## This is the second title
+
+Hello, this is a test. 
+
+Things I want to do: 
+
+- Sleep 
+- Go home 
+- Eat lunch
+
+Another changes 
---
+++ day1/demo copy/sketch.js
@@ -0,0 +1,52 @@
+function setup() {
+    createCanvas(400, 400);
+    
+    let answer = 42; // Declared a variable 
+    let fav = 2; // Number data type
+
+    console.log(answer + fav); // Print out 44
+    console.log(answer - fav);
+    console.log(answer * fav);
+    console.log(answer / fav);
+
+    let fruit1 = 'apple'; // String data type
+    let fruit2 = 'banana'; 
+
+    console.log(fruit1 + ' ' + fruit2); // Print out apple banana
+
+    console.log(`I like ${fruit1}s, but ${fruit2}s no.`); // Print out apple banana
+
+    console.log('cat'[1]); // Print out a. c = 0, t = 2
+
+    console.log(answer.toString());
+
+    let determine = 42==43; // stored value will be a Boolean data type with false value 
+    console.log(42==43);
+
+    const name = 'Amy'; // constant
+    // name = 'iPhone'; // impossible to reassign
+
+    let huh;
+    console.log(huh); // undefined
+
+    let droid = 'BB-8';
+    let fromEarth = null; //Non-applicable
+    console.log(fromEarth);
+ }
+
+function draw() {
+    background(0);
+
+    fill('blue'); // fill color before drawing the circle 
+    //stroke('green'); //needed for next stroke and strokeWeight to not affect this circle
+    //strokeWeight(10);
+    circle(200, 200, 100);
+
+    fill('red');
+    stroke('yellow'); 
+    strokeWeight(5);
+    circle(200, 200, 50);
+
+    noLoop(); // stops the draw function from looping, or helps code run  or smoother
+faster
+}
