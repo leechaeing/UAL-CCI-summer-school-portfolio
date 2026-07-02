7640bb7a82add75171f4d44f74e41c122580f77d
leechaeing <alee27@risd.edu>
2026-07-02 14:46:48 +0100

    saving demo

--- day4/slider_demo/sketch.js
+++ day4/slider_demo/sketch.js
@@ -4,11 +4,13 @@ function setup() {
   createCanvas(100, 100);
 
   // Create a slider and place it at the top of the canvas.
-  slider = createSlider(0, 255);
+  // Set its default value to 0.
+  // Set its step size to 50.
+  slider = createSlider(0, 255, 0, 50);
   slider.position(500, 500);
   slider.size(80);
 
-  describe('A dark gray square with a range slider at the top. The square changes color when the slider is moved.');
+  describe('A black square with a range slider at the top. The square changes color when the slider is moved.');
 }
 
 function draw() {
