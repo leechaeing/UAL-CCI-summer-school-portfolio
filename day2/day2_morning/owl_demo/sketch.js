8c5a6a9dd12a5fcdaded98aeb93b2682563384d5
leechaeing <alee27@risd.edu>
2026-07-02 19:36:17 +0100

    change to owls

--- day2/day2_morning/owl_demo/sketch.js
+++ day2/day2_morning/owl_demo/sketch.js
@@ -1,20 +1,20 @@
+let x = [400, 500, 300, 600, 200, 630, 170, 510, 290, 400];
+let y = [250, 200, 200, 250, 250, 380, 380, 500, 500, 580];
+let g = [120, 200, 50, 70, 150, 100, 200, 90, 250, 0];
+let s = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
 function setup() {
     createCanvas(800, 600);
+    frameRate(2.5);
 }
 
 function draw() {
     background(0);
     translate(0, -25);
-    owl(400, 250, 120, 1);
-    owl(500, 200, 200, 1);
-    owl(300, 200, 50, 1);
-    owl(600, 250, 70, 1);
-    owl(200, 250, 150, 1);
-    owl(630, 380, 100, 1);
-    owl(170, 380, 200, 1);
-    owl(510, 500, 90, 1);
-    owl(290, 500, 250, 1);
-    owl(400, 580, 0, 1);
+
+    for (let i = 0; i < x.length; i++) {
+        owl(x[i], y[i], g[i], s[i]);
+    }
 }
 // based on Lauren Lee McCarthy
 
@@ -23,17 +23,40 @@ function owl (x, y, g, s) {
   push ();  
   translate (x,y);
   scale (s);
-  stroke (g);
-  strokeWeight (70);
-  line (0,-35,0,-65); //body
-  noStroke ();
-  fill(255-g);
-  ellipse (-17.5, -65, 35, 35); // left eye
-  ellipse (17.5, -65, 35, 35); // Right eye
-  arc (0, -65, 70, 70, 0, PI);
-  fill (g);
-  ellipse (-14, -65, 8, 8); // left pupil
-  ellipse (14, -65, 8, 8);  // right pupil
-  quad (0, -58, 4, -51, 0, -44, -4, -51); //beak
+
+  let r = random (1);
+
+  if (r < 0.1) {
+    stroke (0);
+    strokeWeight (70);
+    line (0,-35,0,-65); //body
+
+    noStroke ();
+    fill(0);
+    ellipse (-17.5, -65, 35, 35); // left eye
+    ellipse (17.5, -65, 35, 35); // Right eye
+    arc (0, -65, 70, 70, 0, PI);
+    
+    fill (0);
+    ellipse (-14, -65, 8, 8); // left pupil
+    ellipse (14, -65, 8, 8);  // right pupil
+    quad (0, -58, 4, -51, 0, -44, -4, -51); //beak
+
+  } else {
+    stroke (g);
+    strokeWeight (70);
+    line (0,-35,0,-65); //body
+
+    noStroke();
+    fill(255 - g);
+    ellipse (-17.5, -65, 35, 35);
+    ellipse (17.5, -65, 35, 35); // Right eye
+    arc (0, -65, 70, 70, 0, PI);
+    
+    fill (g);
+    ellipse (-14, -65, 8, 8); // left pupil
+    ellipse (14, -65, 8, 8);  // right pupil
+    quad (0, -58, 4, -51, 0, -44, -4, -51); //beak
+  }
   pop ();
 }
