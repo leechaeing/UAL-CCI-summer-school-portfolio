c80f0e01848d3ef21bd7effea5208fd4ec6e4dd9
leechaeing <alee27@risd.edu>
2026-07-04 22:21:39 +0100

    saving work 3

--- day4/lab2_options2and3/sketch.js
+++ day4/lab2_options2and3/sketch.js
@@ -1,6 +1,8 @@
 let capture;
 let bgSelect;
 let slider;
+let heartButton;
+let starButton;
 
 let background1;
 let background2; 
@@ -18,6 +20,13 @@ let star4;
 
 let ear1; 
 let ear2; 
+
+//reference to button_demo
+let heartCounter = 0; 
+let starCounter = 0; 
+
+let currentSticker = null; 
+let placedStickers = [];
 
 async function setup() {
   background1 = await loadImage('./assets/background1.jpg');
@@ -40,14 +49,13 @@ async function setup() {
   createCanvas(800, 800); 
 
   describe('Video capture from the device webcam.');
-  createCanvas(800, 800);
 
   capture = createCapture(VIDEO);
   capture.size(600, 500);
   capture.hide();
 
   bgSelect = createSelect();
-  bgSelect.position(150, 800);
+  bgSelect.position(300, 200);
   bgSelect.option('background1');
   bgSelect.option('background2');
   bgSelect.option('background3');
@@ -55,11 +63,20 @@ async function setup() {
   describe('A dropdown to pick a background image.');
 
   slider = createSlider(0, 5, 0, 1);
-  slider.position(300, 800);
+  slider.position(300, 300);
   slider.size(90);
   describe('slider to change blur filter.');
-}
 
+  heartButton = createButton('stickers!');
+  heartButton.position(300, 400);
+  heartButton.mousePressed(cycleHeart);
+  describe('A button for heart stickers to decorate.');
+
+  starButton = createButton('more stickers!');
+  starButton.position(300, 500);
+  starButton.mousePressed(cycleStar);
+  describe('A button for star stickers to decorate.');
+}
 
 function draw() {
   let bgChoice = bgSelect.selected();
@@ -83,12 +100,36 @@ function draw() {
 
   let blurriness = slider.value()
   filter(BLUR, blurriness);
+
+  for (let s of placedStickers) {
+    image(s.img, s.x, s.y, 80, 80);
+  }
 }
 
-//button
+function mousePressed() {
+  if(currentSticker) {
+    placedStickers.push({ img: currentSticker, x: mouseX, y: mouseY});
+  }
+}
+
+function cycleHeart() {
+  let heartOptions = [heart1, heart2, heart3, heart4];
+  let index = heartCounter % heartOptions.length; 
+  currentSticker = heartOptions[index];
+  heartCounter += 1; 
+}
+
+function cycleStar() {
+  let starOptions = [star1, star2, star3, star4];
+  let index = starCounter % starOptions.length; 
+  currentSticker = starOptions[index];
+  starCounter += 1; 
+}
+
+//button - ok
 //slider - ok
 //dropdown - ok
 
 //draw
-//stickers
+//stickers - ok
 //change frame - ok
