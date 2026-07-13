2f46d62eb0bcb721671452ae839dd7d63ab95e38
leechaeing <alee27@risd.edu>
2026-07-13 18:38:16 +0100

    saving

--- final_showcase/pixelsorting/sketch.js
+++ final_showcase/pixelsorting/sketch.js
@@ -2,7 +2,7 @@
 
 let img; 
 let sortingDone = false; 
-let threshold = 5; // lower threshold will create more distortion 
+//let threshold = 5; // lower threshold will create more distortion 
 
 async function setup() {
     img = await loadImage('assets/trafficlight.jpg');
@@ -16,9 +16,17 @@ function draw() {
     if(!sortingDone) {
         img.loadPixels(); // load pixels of the image itself 
 
-        // for(let x = 0; x < img.width; x++) {
-        //     sortColumn(x);
-        // }
+        for(let x = 0; x < img.width; x++) {
+            sortColumn(x);
+        }
+
+        for(let y = 0; y<img.width; y++) {
+            sortRow(y);
+        }
+
+        for(let x = 0; x < img.width; x++) {
+            sortColumn(x);
+        }
 
         for(let y = 0; y<img.width; y++) {
             sortRow(y);
@@ -31,67 +39,69 @@ function draw() {
     noLoop(); 
 }
 
-// function sortColumn(x) {
-//     let y = 0; 
-//     while (y < img.height) {
-//         while(y < img.height) {
-//             let index = (x + y*img.width) * 4; //four because each pixel has four values r,g,b,alpha
-//             let r = img.pixels[index + 0];
-//             let g = img.pixels[index + 1];
-//             let b = img.pixels[index + 2];
-//             let bn = brightness(color(r, g, b));
+function sortColumn(x) {
+    let threshold = random(0,30);
+    let y = 0; 
+    while (y < img.height) {
+        while(y < img.height) {
+            let index = (x + y*img.width) * 4; //four because each pixel has four values r,g,b,alpha
+            let r = img.pixels[index + 0];
+            let g = img.pixels[index + 1];
+            let b = img.pixels[index + 2];
+            let bn = brightness(color(r, g, b));
 
-//             if(bn > threshold) { //bn < threshold to switch to dark mode
-//                 break; 
-//             }
-//             y++; 
-//         }
-//         let startY = y; 
+            if(bn > threshold) { //bn < threshold to switch to dark mode
+                break; 
+            }
+            y++; 
+        }
+        let startY = y; 
 
-//         while(y < img.height) {
-//             let index = (x + y*img.width) * 4; //four because each pixel has four values r,g,b,alpha
-//             let r = img.pixels[index + 0];
-//             let g = img.pixels[index + 1];
-//             let b = img.pixels[index + 2];
-//             let bn = brightness(color(r, g, b));
+        while(y < img.height) {
+            let index = (x + y*img.width) * 4; //four because each pixel has four values r,g,b,alpha
+            let r = img.pixels[index + 0];
+            let g = img.pixels[index + 1];
+            let b = img.pixels[index + 2];
+            let bn = brightness(color(r, g, b));
 
-//             if(bn <= threshold) { //bn >= threshold for dark mode
-//                 break; 
-//             }
-//             y++; 
-//         }
-//         let endY = y - 1; 
+            if(bn <= threshold) { //bn >= threshold for dark mode
+                break; 
+            }
+            y++; 
+        }
+        let endY = y - 1; 
 
-//         if(startY < endY) {
-//             let sortingArray = [];
-//             for(let i = startY; i <= endY; i++) {
-//                 let index = (x + i*img.width) *4;
-//                 let r = img.pixels[index + 0];
-//                 let g = img.pixels[index + 1];
-//                 let b = img.pixels[index + 2];
-//                 sortingArray.push(color(r, g, b));
-//             }
+        if(startY < endY) {
+            let sortingArray = [];
+            for(let i = startY; i <= endY; i++) {
+                let index = (x + i*img.width) *4;
+                let r = img.pixels[index + 0];
+                let g = img.pixels[index + 1];
+                let b = img.pixels[index + 2];
+                sortingArray.push(color(r, g, b));
+            }
 
-//             sortingArray.sort((a, b) => brightness(a) - brightness(b)); //sorts from dark to light here, swap a and b for light to dark
+            sortingArray.sort((a, b) => brightness(b) - brightness(a)); //sorts from light to dark
             
-//             for(let i = startY; i <= endY; i++) {
-//                 let index = (x + i*img.width) *4;
-//                 let c = sortingArray[i - startY];
-//                 img.pixels[index + 0] = red(c); 
-//                 img.pixels[index + 1] = green(c);
-//                 img.pixels[index + 2] = blue(c);
-//                 img.pixels[index + 3] = 255;
-//             }
-//             y++
-//         }
-//     }
+            for(let i = startY; i <= endY; i++) {
+                let index = (x + i*img.width) *4;
+                let c = sortingArray[i - startY];
+                img.pixels[index + 0] = red(c); 
+                img.pixels[index + 1] = green(c);
+                img.pixels[index + 2] = blue(c);
+                img.pixels[index + 3] = 255;
+            }
+            y++
+        }
+    }
 
-// }
+}
 
 function sortRow(y) {
+    let threshold = random(0,30);
     let x = 0; 
-    while (x < img.height) {
-        while(x < img.height) {
+    while (x < img.width) {
+        while(x < img.width) {
             let index = (x + y*img.width) * 4; //four because each pixel has four values r,g,b,alpha
             let r = img.pixels[index + 0];
             let g = img.pixels[index + 1];
@@ -105,7 +115,7 @@ function sortRow(y) {
         }
         let startX = x; 
 
-        while(y < img.height) {
+        while(x < img.width) {
             let index = (x + y*img.width) * 4; //four because each pixel has four values r,g,b,alpha
             let r = img.pixels[index + 0];
             let g = img.pixels[index + 1];
@@ -129,7 +139,7 @@ function sortRow(y) {
                 sortingArray.push(color(r, g, b));
             }
 
-            sortingArray.sort((a, b) => brightness(a) - brightness(b)); //sorts from dark to light here, swap a and b for light to dark
+            sortingArray.sort((a, b) => brightness(a) - brightness(b)); //sorts from  dark to light here 
             
             for(let i = startX; i <= endX; i++) {
                 let index = (x + i*img.width) *4;
